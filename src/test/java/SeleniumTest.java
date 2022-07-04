f17dfef6db04617d2d5a902b397fb8ff76f1a164
mk tester automatyczny <mk4testy@gmail.com>
2022-07-04 20:44:06 +0200

    Add updates

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,8 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class SeleniumTest {
     public static void main(String[] args) {
 
-        System.out.println("Io");
-
+        WebDriver driver = new ChromeDriver();
 
     }
+
+    @Test
+    public void sampleTest() {
+        System.out.println("Hello");
+    }
+
 }
