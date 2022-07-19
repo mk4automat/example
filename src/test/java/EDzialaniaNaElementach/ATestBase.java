2015e6c2b4e19ee86406edf517529ea8ac060386
mk tester automatyczny <mk4testy@gmail.com>
2022-07-19 09:27:40 +0200

    Update class and add Dzialania na elementach

--- src/test/java/EDzialaniaNaElementach/ATestBase.java
+++ src/test/java/EDzialaniaNaElementach/ATestBase.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class ATestBase {
--- src/test/java/EDzialaniaNaElementach/EBasicActionsTest.java
+++ src/test/java/EDzialaniaNaElementach/EBasicActionsTest.java
@@ -4,7 +4,10 @@ import net.bytebuddy.asm.Advice;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class EBasicActionsTest extends ATestBase {
 
@@ -30,7 +33,7 @@ public class EBasicActionsTest extends ATestBase {
         usernameField.sendKeys("nowyUser");
 
         //Przycisk na klawiaturze Enter
-        usernameField.sendKeys(Keys.ENTER);
+        //usernameField.sendKeys(Keys.ENTER);
         //Przycisk na klawiaturze Tab
         //usernameField.sendKeys(Keys.TAB);
 
@@ -45,6 +48,7 @@ public class EBasicActionsTest extends ATestBase {
 
         By linkTekst = By.linkText("Podstawowa strona testowa");
         WebElement basicPageLink = driver.findElement(linkTekst);
+        System.out.println(basicPageLink.getText());
         basicPageLink.click();
 
         //Checkbox
@@ -55,6 +59,64 @@ public class EBasicActionsTest extends ATestBase {
         //Select
         WebElement selectCar = driver.findElement(By.cssSelector("select > option[value='volvo']"));
         selectCar.click();
+        //Można też wybrać po klasie select (deselect - odznaczenie)
+        WebElement selectCars = driver.findElement(By.cssSelector("select"));
+        Select cars = new Select(selectCars);
+        cars.selectByVisibleText("Saab");
+
+        //Pobranie textu ze znaczników option z selecta
+        List<WebElement> options = cars.getOptions();
+
+        for (WebElement jakasNazwa : options) {
+            System.out.println(jakasNazwa.getText());
+        }
+    }
+
+    @Test
+    public void performAction3() {
+
+        driver.get("https://testeroprogramowania.github.io/selenium/");
+
+        //Kliknięcie w element
+        By linkTekst = By.linkText("Podstawowa strona testowa");
+        WebElement basicPageLink = driver.findElement(linkTekst);
+        basicPageLink.click();
+
+
+        //Usunięcie wartości i wpisanie nowej
+        WebElement usernameField = driver.findElement(By.name("username"));
+        usernameField.clear();
+        usernameField.sendKeys("nowyUser");
+
+        //Pobranie wartości z pola tekstowego
+        System.out.println(usernameField.getAttribute("value"));
+
+        //Pobranie wartości z ukrytego elementu (działa "textContent")
+        WebElement para = driver.findElement(By.className("topSecret"));
+        System.out.println("By text " + para.getText());
+        System.out.println("By attr value: " + para.getAttribute("value"));
+        System.out.println("By text content: " + para.getAttribute("textContent"));
+
+    }
+
+
+    @Test
+    public void performAction4() {
+
+        driver.get("https://testeroprogramowania.github.io/selenium/");
+
+        //Kliknięcie w element
+        By linkTekst = By.linkText("Podstawowa strona testowa");
+        WebElement basicPageLink = driver.findElement(linkTekst);
+        basicPageLink.click();
+
+        //Wyświetlenie alertu
+        WebElement button = driver.findElement(By.id("clickOnMe"));
+        button.click();
+
+        //Alerty: sa tylko 3 opcje [accept(), sendKeys(), dismiss() po poleceniu "driver.switchTo().alert()."]
+
+        driver.switchTo().alert().accept();
 
 
     }
