158c838845c81d7462251325fc7b9ab55c869c01
mk tester automatyczny <mk4testy@gmail.com>
2022-07-20 14:38:23 +0200

    Add IFrameTest class

--- src/test/java/EDzialaniaNaElementach/ENewWindowTest.java
+++ src/test/java/EDzialaniaNaElementach/ENewWindowTest.java
@@ -4,22 +4,38 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import java.util.Set;
+
 public class ENewWindowTest extends ATestBase{
 
     @Test
-    public void performAction4() {
+    public void testNewWindow() {
 
-        driver.get("https://testeroprogramowania.github.io/selenium/");
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
 
-        //Kliknięcie w element
-        By linkTekst = By.linkText("Podstawowa strona testowa");
-        WebElement basicPageLink = driver.findElement(linkTekst);
-        basicPageLink.click();
+        //Nowe okno przeglądarki ("driver.switchTo().window("nazwa");)
 
-
-        //Nowe okno przeglądarki
+        //Najpierw pobranie nazwy okna w którym jestem
+        String currentWindow = driver.getWindowHandle();
+        //Otworzenie nowego okna
         WebElement newPageButton = driver.findElement(By.id("newPage"));
         newPageButton.click();
+        //Pobranie nazw wszystkich okien
+        Set<String> windowNames = driver.getWindowHandles();
+        //Sprawdzenie czy jest inna nazwa niż currentWindow, jeżeli tak, to sie do niej przełączamy
+        for (String window: windowNames) {
+            if (!window.equals(currentWindow)) {
+                driver.switchTo().window(window);
+            }
+        }
+        //Wyszukanie elementów w nowym oknie
+        driver.findElement(By.id("L2AGLb")).click();
+        driver.findElement(By.name("q")).sendKeys("Selenium");
+
+        //Powrót do wcześniejszego okna
+        driver.switchTo().window(currentWindow);
+        //Znalezienie elementu w pierwszym oknie
+        driver.findElement(By.name("username")).sendKeys("XXXXX");
 
     }
 }
---
+++ src/test/java/EDzialaniaNaElementach/IFrameTest.java
@@ -0,0 +1,15 @@
+package EDzialaniaNaElementach;
+
+import org.testng.annotations.Test;
+
+public class IFrameTest extends ATestBase{
+
+    @Test
+    public void testIFrame() {
+
+        driver.get("https://testeroprogramowania.github.io/selenium/iframe.html");
+
+        //
+
+    }
+}
